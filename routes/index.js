dce8d00dae39b0abffbfb2c87b74be1681140939
joje <joje.attrs@gmail.com>
2023-06-15 01:59:55 +0900

    ..

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,17 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.render('index.html');
+  bcrypt.genSalt(10, function (err, salt) {
+    if (err) return next(err);
+
+    bcrypt.hash('password', salt, function (err, hash) {
+      if (err) return next(err);
+
+      // Store hash in your password DB.
+      res.render('index.html', hash);
+    });
+  });
+
 });
 
 module.exports = router;
