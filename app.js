41f4eee423fef55076ed90f112ad4f10a8a70b43
joje <joje.attrs@gmail.com>
2023-07-18 03:36:24 +0900

    fix

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const nunjucks = require('nunjucks');
 
 const indexRouter = require('./routes/index');
 // var usersRouter = require('./routes/users');
@@ -11,14 +9,6 @@ const indexRouter = require('./routes/index');
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'njk');
-nunjucks.configure('views', {
-  express: app,
-  watch: true,
-});
-
-app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
--- routes/index.js
+++ routes/index.js
@@ -4,20 +4,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  console.log('main');
-
-  bcrypt.genSalt(10, function (err, salt) {
-    if (err) return next(err);
-
-    bcrypt.hash('password', salt, function (err, hash) {
-      if (err) return next(err);
-
-      // Store hash in your password DB.
-      console.log('hash', hash);
-      res.render('index.html');
-    });
-  });
-
+  res.send({ data: 'hello world' });
 });
 
 module.exports = router;
