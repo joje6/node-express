b89ce680a131694b2f49877854ec514e25b71b77
joje <joje6@users.noreply.github.com>
2023-09-27 03:04:54 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  res.send({ data: 'hello world!' });
+  res.send({ data: 'hello world.' });
 });
 
 router.get('/test', (req, res, next) => {
