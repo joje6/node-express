b3e0e0963d02cdc0e68a1e943e5d7923233574bc
joje <joje6@users.noreply.github.com>
2024-08-12 13:53:49 +0900

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -11,4 +11,12 @@ router.get('/test', (req, res, next) => {
   res.send(req.query);
 });
 
+router.get('/status/403', (req, res, next) => {
+  res.status(403).send({test: 'ok'});
+});
+
+router.get('/status/502', (req, res, next) => {
+  res.status(403).send({test: 'ok'});
+});
+
 module.exports = router;
